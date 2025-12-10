bef997b20cf76f4b43624e2e0c37747dc1854aa1
kanomitri <kanomitri@gmail.com>
2025-12-10 17:12:56 -0500

    Final mods for item customization and menu order.

---
+++ target/classes/com/autobots/ItemCustomizationPopUpController.java
@@ -0,0 +1,25 @@
+package com.autobots;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ItemCustomizationPopUpController {
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    void cancelModifications(ActionEvent event) {
+        
+    }
+
+    @FXML
+    void saveOptions(ActionEvent event) {
+
+    }
+
+}
