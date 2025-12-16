ecffc7e844627a414f01df10ab0b80fb684ae5b2
Xay <xjohnson399@gmail.com>
2025-12-16 05:37:47 -0500

    feat: Update FXML controllers to new package structure and add module requirements

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,14 @@ module com.autobots {
     requires javafx.fxml;
     requires java.sql;
     requires mysql.connector.j;
+    requires com.gluonhq.charm.glisten;
     
 
-    opens com.autobots to javafx.fxml;
+    //fix: opens the new contorller package for FXML access
+    opens com.autobots.controller to javafx.fxml;
+
+    //fix: opens the model package to be used in TableView/ListView(FX properties)
+    opens com.autobots.model to javafx.base;
+
     exports com.autobots;
 }
